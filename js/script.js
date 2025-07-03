98a5c9569f42c5c4b37460f9ec0a39846340c3d5
bezerra9 <gabrielbezerragof@gmail.com>
2025-07-03 20:46:51 -0300

    commit-adicionando-uma-melhor-experiencia-no-formulario-com-JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+
+// ativar links do header
 const links = document.querySelectorAll('.header-menu a')
 
 links.forEach(ativarLink)
@@ -5,8 +8,21 @@ links.forEach(ativarLink)
 function ativarLink(link) {
   const url = location.href
   const href = link.href
-
   if(url.includes(href)) {
     link.classList.add('ativo')
   }
 }
+
+// ativar itens do Orçamento
+
+const parametros = new URLSearchParams(location.search) 
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+  if(elemento) {
+    elemento.checked = true  
+  }
+  console.log(parametro)
+}
+
+parametros.forEach(ativarProduto)
