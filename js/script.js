bee17d4a2c03dcf5723fa1df80c4bc7449cd676b
bezerra9 <gabrielbezerragof@gmail.com>
2025-07-04 12:33:40 -0300

    commit-da-bicicleta-imagem-js

--- js/script.js
+++ js/script.js
@@ -48,3 +48,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas)
+
+// galeria de bicicleta
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches
+  if(media) {
+    galeriaContainer.prepend(img)
+  }
+  console.log(media)
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
