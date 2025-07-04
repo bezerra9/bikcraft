ccf177e9178fa44a2e436a38c52cd99d3837402e
bezerra9 <gabrielbezerragof@gmail.com>
2025-07-04 12:21:05 -0300

    commit-das-perguntas-frequentes-js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function ativarLink(link) {
 
 // ativar itens do Orçamento
 
-const parametros = new URLSearchParams(location.search) 
+const parametros = new URLSearchParams(location.search)
 
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro)
@@ -26,3 +26,25 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto)
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button")
+/* console.log(perguntas) */
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+  resposta.classList.toggle('ativa')
+  const ativa = resposta.classList.contains("ativa")
+  pergunta.setAttribute('aria-expanded', ativa)
+  console.log(controls)
+}
+
+function eventosPerguntas(pergunta) {
+  /* console.log(pergunta) */
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
